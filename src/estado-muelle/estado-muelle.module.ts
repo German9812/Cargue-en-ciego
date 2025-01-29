3d23a73706a49c9437432926a47b402b022214d5
German <german.soto@altipal.com.co>
2025-01-29 15:13:42 -0500

    actualización entidad estado muelle

--- src/estado-muelle/estado-muelle.module.ts
+++ src/estado-muelle/estado-muelle.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EstadoMuelleService } from './estado-muelle.service';
 import { EstadoMuelleController } from './estado-muelle.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EstadoMuelleService])],
   controllers: [EstadoMuelleController],
   providers: [EstadoMuelleService],
 })
--- src/estado-muelle/estado-muelle.service.ts
+++ src/estado-muelle/estado-muelle.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEstadoMuelleDto } from './dto/create-estado-muelle.dto';
 import { UpdateEstadoMuelleDto } from './dto/update-estado-muelle.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EstadoMuelle } from './entities/estado-muelle.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EstadoMuelleService {
-  create(createEstadoMuelleDto: CreateEstadoMuelleDto) {
-    return 'This action adds a new estadoMuelle';
+  constructor(
+    @InjectRepository(EstadoMuelle)
+    private readonly estadoMuelleRepository: Repository<EstadoMuelle>,
+  ) {}
+
+async create(createEstadoMuelleDto: CreateEstadoMuelleDto) {
+  const { vehiculoId, muelleId, ...rest } = createEstadoMuelleDto;
+
+  const estadoMuelle = this.estadoMuelleRepository.create({
+    ...rest,
+  });
+
+  if (vehiculoId) estadoMuelle.vehiculo = { Id_vehiculo: vehiculoId } as any;
+  if (muelleId) estadoMuelle.muelle = { Id_muelle: muelleId } as any;
+
+  return this.estadoMuelleRepository.save(estadoMuelle);
+}
+
+
+  async findAll(): Promise<EstadoMuelle[]> {
+    return await this.estadoMuelleRepository.find({
+      relations: ['vehiculo', 'muelle'],
+    });
   }
 
-  findAll() {
-    return `This action returns all estadoMuelle`;
+  async findOne(id: number): Promise<EstadoMuelle> {
+    const estadoMuelle = await this.estadoMuelleRepository.findOne({
+      where: { Id_estado_Muelle: id },
+      relations: ['vehiculo', 'muelle'],
+    });
+    if (!estadoMuelle) {
+      throw new NotFoundException(`EstadoMuelle con ID ${id} no encontrado`);
+    }
+    return estadoMuelle;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} estadoMuelle`;
+  async update(id: number, updateEstadoMuelleDto: UpdateEstadoMuelleDto): Promise<EstadoMuelle> {
+    const estadoMuelle = await this.findOne(id);
+    const { vehiculoId, muelleId, ...rest } = updateEstadoMuelleDto;
+
+    if (vehiculoId) estadoMuelle.vehiculo = { Id_vehiculo: vehiculoId } as any;
+    if (muelleId) estadoMuelle.muelle = { Id_muelle: muelleId } as any;
+
+    Object.assign(estadoMuelle, rest);
+
+    return this.estadoMuelleRepository.save(estadoMuelle);
   }
 
-  update(id: number, updateEstadoMuelleDto: UpdateEstadoMuelleDto) {
-    return `This action updates a #${id} estadoMuelle`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} estadoMuelle`;
+  async remove(id: number): Promise<void> {
+    const estadoMuelle = await this.estadoMuelleRepository.delete(id);
+    if (estadoMuelle.affected === 0) {
+      throw new NotFoundException(`EstadoMuelle con ID ${id} no encontrado`);
+    }
   }
 }
